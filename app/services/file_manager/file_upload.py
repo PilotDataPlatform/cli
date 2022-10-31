d3817aa10d1e6b4f7d69a3d376ea4632968b77b4
zhiren <zzhan@indocresearch.org>
2022-10-31 17:36:36 -0400

    adapot the new restriction rules
    Former-commit-id: e201881b769863267ce4839cc779c98bbfbfc444

--- app/services/file_manager/file_upload.py
+++ app/services/file_manager/file_upload.py
@@ -150,9 +150,10 @@ class SrvSingleFileUploader(metaclass=MetaService):
     @require_valid_token()
     def pre_upload(self):
         url = (
-            AppConfig.Connections.url_bff + '/v1/project/{}/files'.format(self.project_code)
-            if self.zone == AppConfig.Env.core_zone
-            else self.base_url + '/v1/files/jobs'
+            AppConfig.Connections.url_bff
+            + '/v1/project/{}/files'.format(self.project_code)
+            # if self.zone == AppConfig.Env.core_zone
+            # else self.base_url + '/v1/files/jobs'
         )
         payload = uf.generate_pre_upload_form(
             self.project_code,
