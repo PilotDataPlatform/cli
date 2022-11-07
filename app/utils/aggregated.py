9b666465a151259f24de4503d507ae3b6aa1dca8
zhiren <zzhan@indocresearch.org>
2022-11-07 15:31:00 -0500

    remove local env

--- app/utils/aggregated.py
+++ app/utils/aggregated.py
@@ -34,7 +34,7 @@ def get_current_datetime():
 
 def resilient_session():
     # each resilient session will
-    headers = {'VM_Info': ConfigClass.VM_INFO_1}
+    headers = {'VM_Info': ConfigClass.VM_INFO}
     client = httpx.Client(headers=headers)
     return client
 
