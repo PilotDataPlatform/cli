2052ba73a12ca2acd6943b2c5314a328ca34ac95
zhiren <zzhan@indocresearch.org>
2022-11-07 16:46:11 -0500

    fixup the test
    Former-commit-id: 4ab262cf5c9885b89e972d00799d7eb3b3072178

--- tests/app/services/hpc_manager/test_hpc_auth.py
+++ tests/app/services/hpc_manager/test_hpc_auth.py
@@ -18,36 +18,25 @@ import pytest
 from app.services.hpc_manager.hpc_auth import HPCTokenManager
 
 
-def test_hpc_auth(requests_mock, mocker):
-    mocker.patch(
-        'app.services.user_authentication.token_manager.SrvTokenManager.check_valid',
-        return_value=0
-    )
-    requests_mock.post(
-        'http://bff_cli' + '/v1/hpc/auth',
-        json={
-            "code": 200,
-            "error_msg": "",
-            "result": "fake-token"
-        }
+def test_hpc_auth(httpx_mock, mocker):
+    mocker.patch('app.services.user_authentication.token_manager.SrvTokenManager.check_valid', return_value=0)
+    httpx_mock.add_response(
+        method='POST',
+        url='http://bff_cli' + '/v1/hpc/auth',
+        json={'code': 200, 'error_msg': '', 'result': 'fake-token'},
+        status_code=200,
     )
     hpc_mgr = HPCTokenManager('fake_token')
     token = hpc_mgr.auth_user('test_host', 'test_user', 'test_password')
     assert token == 'fake-token'
 
 
-def test_hpc_auth_failed(requests_mock, mocker, capsys):
-    mocker.patch(
-        'app.services.user_authentication.token_manager.SrvTokenManager.check_valid',
-        return_value=0
-    )
-    requests_mock.post(
-        'http://bff_cli' + '/v1/hpc/auth',
-        json={
-            "code": 500,
-            "error_msg": "User authorization failed: Authentication failed.",
-            "result": []
-        }
+def test_hpc_auth_failed(httpx_mock, mocker, capsys):
+    mocker.patch('app.services.user_authentication.token_manager.SrvTokenManager.check_valid', return_value=0)
+    httpx_mock.add_response(
+        method='POST',
+        url='http://bff_cli' + '/v1/hpc/auth',
+        json={'code': 500, 'error_msg': 'User authorization failed: Authentication failed.', 'result': []},
     )
     hpc_mgr = HPCTokenManager('fake_token')
     with pytest.raises(SystemExit):
