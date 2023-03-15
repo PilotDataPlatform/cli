f1f1a39467d34204776702349e0b4f1b6c2f5629
zhiren <zzhan@indocresearch.org>
2023-03-15 09:53:26 -0400

    update token refresh to a standalone thread

--- app/services/file_manager/file_upload/file_upload.py
+++ app/services/file_manager/file_upload/file_upload.py
@@ -175,15 +175,22 @@ def simple_upload(  # noqa: C901
 
     # now loop over each file under the folder and start
     # the chunk upload
-    pool = ThreadPool(num_of_thread)
+
+    # thread number +1 reserve one thread to refresh token
+    # and remove the token decorator in functions
+
+    pool = ThreadPool(num_of_thread + 1)
+    pool.apply_async(upload_client.upload_token_refresh)
     for file_object in pre_upload_infos:
         upload_client.stream_upload(file_object, pool)
-        # TODO: if there is some racing error make the combine chunks
+        # NOTE: if there is some racing error make the combine chunks
         # out of thread pool.
         pool.apply_async(
             upload_client.on_succeed,
             args=(file_object, tags),
         )
+
+    upload_client.set_finish_upload()
 
     pool.close()
     pool.join()
