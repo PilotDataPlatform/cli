dfcc6895a9f053e1a9273bc5a3cd85d4b6181057
zhiren <zzhan@indocresearch.org>
2023-04-04 10:06:46 -0400

    add more test case for upload client models
    Former-commit-id: a773a280d040e9221f857a821d8af42e7198f057

---
+++ tests/app/services/file_manager/file_upload/test_model.py
@@ -0,0 +1,36 @@
+# Copyright (C) 2022-2023 Indoc Research
+#
+# Contact Indoc Research for any questions regarding the use of this source code.
+
+from app.configs.app_config import AppConfig
+from app.services.file_manager.file_upload.models import FileObject
+
+
+def test_file_upload_model_update_progress_bar(mocker):
+    mocker.patch('os.path.getsize', return_value=100)
+
+    file_obj = FileObject('test', 'test', 'test', 'test', 'test', [])
+    file_obj.update_progress(1)
+
+    assert file_obj.progress_bar is not None
+    assert file_obj.progress_bar.n == 1
+
+
+def test_file_upload_model_close_progress_bar(mocker):
+    mocker.patch('os.path.getsize', return_value=100)
+
+    file_obj = FileObject('test', 'test', 'test', 'test', 'test', [])
+    file_obj.close_progress()
+
+    assert file_obj.progress_bar is None
+
+
+def test_file_upload_model_generate_meta(mocker):
+    AppConfig.Env.chunk_size = 10
+    mocker.patch('os.path.getsize', return_value=100)
+
+    file_obj = FileObject('test', 'test', 'test', 'test', 'test', [])
+    total_size, total_chunks = file_obj.generate_meta('test')
+
+    assert total_size == 100
+    assert total_chunks == (100 / 10)
