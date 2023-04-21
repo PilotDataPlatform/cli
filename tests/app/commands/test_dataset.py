48e95430ac026f897d10ace0109fc81d12b76914
QXgu <amy.gubca@gmail.com>
2023-04-20 22:05:29 -0400

    Fix unit test name
    Former-commit-id: 97590995fdb313c08fa0052061692ed913043355

--- tests/app/commands/test_dataset.py
+++ tests/app/commands/test_dataset.py
@@ -6,7 +6,7 @@ from app.commands.dataset import dataset_download
 from app.configs.app_config import AppConfig
 
 
-def test_file_upload_command_success_with_attribute(requests_mock, mocker, cli_runner, capsys):
+def test_download_not_exited_dataset_version(requests_mock, mocker, cli_runner, capsys):
     mocker.patch('app.services.user_authentication.token_manager.SrvTokenManager.check_valid', return_value=0)
     requests_mock.get(
         'http://bff_cli' + '/v1/dataset/testdataset',
