2091e134dfdced14c5f9853f03f879772f599907
Vadym Moshynskyi <vmoshynskyi@indocresearch.org>
2023-06-07 16:05:30 +0200

    PILOT-3002: Remove login using username and password (#65)
    * PILOT-3002: Bump cryptography
    
    * PILOT-3002: Remove login using username and password

--- tests/app/services/user_authentication/test_user_login_logout.py
+++ tests/app/services/user_authentication/test_user_login_logout.py
@@ -6,64 +6,13 @@ import pytest
 
 from app.configs.app_config import AppConfig
 from app.configs.user_config import UserConfig
-from app.resources.custom_error import Error
-from app.services.output_manager.error_handler import ECustomizedError
 from app.services.user_authentication.user_login_logout import check_is_login
 from app.services.user_authentication.user_login_logout import user_device_id_login
-from app.services.user_authentication.user_login_logout import user_login
 from app.services.user_authentication.user_login_logout import validate_user_device_login
 
 
-def test_user_login_success(requests_mock):
-    requests_mock.post(
-        'http://service_auth',
-        json={
-            'code': 200,
-            'error_msg': '',
-            'page': 0,
-            'total': 1,
-            'num_of_pages': 1,
-            'result': {
-                'access_token': 'fake-token',
-                'expires_in': 300,
-                'refresh_expires_in': 360,
-                'refresh_token': 'refresh-token',
-                'token_type': 'Bearer',
-                'not-before-policy': 0,
-                'session_state': 'session-state',
-                'scope': 'roles groups profile email',
-            },
-        },
-    )
-    res = user_login('username', 'password')
-    assert res.get('code') == 200
-    assert res['result'].get('access_token') == 'fake-token'
-    assert res['result'].get('refresh_token') == 'refresh-token'
-    assert res.get('error_msg') == ''
-
-
-def test_user_login_wrong_password(requests_mock, capsys):
-    requests_mock.post(
-        'http://service_auth',
-        json={
-            'code': 401,
-            'error_msg': "401: b'{\"error\":\"invalid_grant\",\"error_description\":\"Invalid user credentials\"}'",
-            'page': 0,
-            'total': 1,
-            'num_of_pages': 1,
-            'result': [],
-        },
-        status_code=401,
-    )
-    with pytest.raises(SystemExit):
-        user_login('username', 'password')
-    out, err = capsys.readouterr()
-    assert out == Error.error_msg.get(ECustomizedError.INVALID_CREDENTIALS.name, 'Unknown error.') + '\n'
-    assert err == ''
-
-
 def test_check_is_not_login(mocker):
-    mocker.patch('configparser.ConfigParser.has_option', return_value=False)
+    mocker.patch('app.configs.user_config.UserConfig.is_logged_in', return_value=False)
     expected_result = False
     with pytest.raises(SystemExit):
         actual = check_is_login()
