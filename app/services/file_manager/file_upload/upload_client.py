1674f4bb0b917208df2d18c80d73e456b193674a
Matvey Loshakov <mloshakov@indocresearch.org>
2023-08-08 12:16:48 +0200

    [IHDC-354] Update copyright statement

--- app/services/file_manager/file_upload/upload_client.py
+++ app/services/file_manager/file_upload/upload_client.py
@@ -1,6 +1,6 @@
-# Copyright (C) 2022-2023 Indoc Research
+# Copyright (C) 2022-2023 Indoc Systems
 #
-# Contact Indoc Research for any questions regarding the use of this source code.
+# Contact Indoc Systems for any questions regarding the use of this source code.
 
 import hashlib
 import json
