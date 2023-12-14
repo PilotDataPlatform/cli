0e051a0276c743d3688bed2bdba784714059fb6c
zhiren <zzhan@indocresearch.org>
2023-12-14 12:07:50 -0500

    hotfix the metadata use wrong input as container code
    Former-commit-id: 2e1962835cfe2887568adbedc74024fcadd41c23

--- tests/app/services/file_manager/file_metadata/test_file_metadata_client.py
+++ tests/app/services/file_manager/file_metadata/test_file_metadata_client.py
@@ -49,3 +49,38 @@ def test_file_metadata_client_get_detail_success(mocker, httpx_mock):
     assert item_info == item_info
     assert res_attributes == {attri_template_name: attributes.get(attri_template_uid)}
     assert tags == tags
+
+
+def test_file_metadata_client_get_detail_success_with_no_tag_attributes(mocker, httpx_mock):
+    item_info = {
+        'id': 'test',
+        'parent_id': 'test_parent',
+        'parent_path': '',
+        'name': 'admin',
+        'zone': 0,
+        'status': 'ACTIVE',
+    }
+
+    mocker.patch(
+        'app.services.user_authentication.token_manager.SrvTokenManager.decode_access_token',
+        return_value=decoded_token(),
+    )
+
+    mocker.patch(
+        'app.services.file_manager.file_metadata.file_metadata_client.search_item',
+        return_value={'result': {**item_info, 'extended': {'extra': {'tags': [], 'attributes': {}}}}},
+    )
+
+    mocker.patch(
+        'app.services.file_manager.file_metadata.file_metadata_client.FileMetaClient.save_file_metadata',
+        return_value=None,
+    )
+
+    file_meta_client = FileMetaClient('zone', 'project_code/object_path', 'general', 'attr', 'tag')
+    assert file_meta_client.project_code == 'project_code'
+    assert file_meta_client.object_path == 'object_path'
+
+    item_info, res_attributes, tags = file_meta_client.download_file_metadata()
+    assert item_info == item_info
+    assert res_attributes == {}
+    assert tags == tags
