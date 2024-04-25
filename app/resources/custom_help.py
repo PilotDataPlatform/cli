c7f8d90c92a7442a0dcb40f9095a107897fd9e02
Color Zhan <colorzzr@gmail.com>
2024-04-25 14:33:28 -0400

    Pilot 4951: Update the logic of upload command for root level folder(eg. users, shared) (#134)
    * add [p] prefix for project folder when listing items under project
    
    * group itemprefix class with itemtype class
    
    * add double quotation when item name contains space
    
    * change enum type PROJECTFOLDER to SHAREDFOLDER
    
    * replace PROJECTFOLDER with SHAREDFOLDER
    
    * fixup test cases
    
    * fixup logic for different prefix
    
    * add prefix for name folder
    
    * bump up version to 3.0.0
    
    * fixup test cases
    
    * fixup test cases
    
    * replace hardcoded string with enum value in test cases
    
    * remove the reference of project_folder
    
    * fixup test cases
    
    * update help page
    
    ---------
    
    Co-authored-by: zhiren <zzhan@indocresearch.org>
    Former-commit-id: 74b12c15417dd9af47823cf8451c463b6ea128ee

--- app/resources/custom_help.py
+++ app/resources/custom_help.py
@@ -9,9 +9,9 @@ class HelpPage:
     page = {
         'update': {
             'version': pkg_resources.get_distribution('app').version,
-            '1': 'Add new feature for folder merging',
-            '2': 'Secure the config file',
-            '3': 'Optimize logic, input and error message',
+            '1': 'Add a new feature for file metadata downloading.',
+            '2': 'Add a new feature for file moving/renaming.',
+            '3': 'Support shared folder within project.',
         },
         'dataset': {
             'DATASET_DOWNLOAD': 'Download a dataset or a particular version of a dataset.',
