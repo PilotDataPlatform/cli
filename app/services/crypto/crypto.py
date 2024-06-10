f1788d0c165230a2971e6782616f7dc427a8d98f
zhiren <colorzzr@gmail.com>
2024-06-10 11:01:39 -0400

    update crypto iterations

--- app/services/crypto/crypto.py
+++ app/services/crypto/crypto.py
@@ -36,7 +36,7 @@ def encryption(message_to_encrypt, secret):
         algorithm=hashes.SHA256(),
         length=32,
         salt=base64.b64decode(secret),  # input binary string secret key
-        iterations=100000,
+        iterations=600000,
         backend=default_backend(),
     )
     # generate key to involve current device information
