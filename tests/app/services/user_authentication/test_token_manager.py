9376e1ab8b2a4a31bce04abc36bb9248095e9ff7
zhiren <colorzzr@gmail.com>
2024-07-11 16:26:24 -0400

    add test cases for new token refresning logic

--- tests/app/services/user_authentication/test_token_manager.py
+++ tests/app/services/user_authentication/test_token_manager.py
@@ -2,6 +2,8 @@
 #
 # Contact Indoc Systems for any questions regarding the use of this source code.
 
+import time
+
 import jwt
 import pytest
 
@@ -24,7 +26,7 @@ class TestSrvTokenManager:
 
         manager.refresh(fake.pystr())
 
-    def test_refresh_failed_with_invalid_token(self, requests_mock, mocker, settings, capsys):
+    def test_refresh_failed_with_invalid_access_refresh_token_apikey(self, requests_mock, mocker, settings, capsys):
         manager = SrvTokenManager()
         mocker.patch(
             'app.services.user_authentication.token_manager.SrvTokenManager.decode_access_token',
@@ -43,5 +45,64 @@ class TestSrvTokenManager:
         with pytest.raises(SystemExit):
             manager.refresh('test_azp')
         out, _ = capsys.readouterr()
+
         assert out.rstrip() == 'Your login session has expired. Please try again or log in again.'
+        assert login_using_api_key_mock.call_count == 1
+
+    def test_check_valid_return_0_with_valid_access_token(self):
+        user_config = UserConfig()
+        user_config.access_token = jwt.encode(
+            {'aud': 'api-key', 'exp': time.time() + 100 + AppConfig.Env.token_warn_need_refresh, 'azp': 'test'}, key=''
+        )
+
+        manager = SrvTokenManager()
+        assert manager.check_valid('test') == 0
+
+    def test_check_valid_return_1_when_need_refresh(self):
+        user_config = UserConfig()
+        user_config.access_token = jwt.encode(
+            {'aud': 'api-key', 'exp': time.time() + AppConfig.Env.token_warn_need_refresh, 'azp': 'test'}, key=''
+        )
+
+        manager = SrvTokenManager()
+        assert manager.check_valid('test') == 1
+
+    def test_check_valid_return_1_when_access_expire_with_refresh_token(self):
+        user_config = UserConfig()
+        user_config.access_token = jwt.encode({'aud': 'api-key', 'exp': time.time() - 100, 'azp': 'test'}, key='')
+        user_config.refresh_token = jwt.encode({'refresh': 'token', 'exp': time.time() + 100}, key='')
+
+        manager = SrvTokenManager()
+        assert manager.check_valid('test') == 1
+
+    def test_check_valid_return_1_when_access_refresh_token_expired_with_api_key(self, mocker):
+        user_config = UserConfig()
+        user_config.access_token = jwt.encode({'aud': 'api-key', 'exp': time.time() - 100, 'azp': 'test'}, key='')
+        user_config.refresh_token = jwt.encode({'refresh': 'token', 'exp': time.time() - 100}, key='')
+        user_config.api_key = 'test'
+        login_using_api_key_mock = mocker.patch(
+            'app.services.user_authentication.token_manager.login_using_api_key', return_value=True
+        )
+
+        manager = SrvTokenManager()
+        assert manager.check_valid('test') == 1
+        assert login_using_api_key_mock.call_count == 1
+
+    def test_check_valid_return_system_error_when_all_expired(self, mocker):
+        user_config = UserConfig()
+        user_config.access_token = jwt.encode({'aud': 'api-key', 'exp': time.time() - 100, 'azp': 'test'}, key='')
+        user_config.refresh_token = jwt.encode({'refresh': 'token', 'exp': time.time() - 100}, key='')
+        user_config.api_key = 'test'
+        login_using_api_key_mock = mocker.patch(
+            'app.services.user_authentication.token_manager.login_using_api_key', return_value=True
+        )
+
+        try:
+            manager = SrvTokenManager()
+            manager.check_valid('test')
+        except SystemError:
+            pass
+        else:
+            assert AssertionError('SystemError not raised')
+
         assert login_using_api_key_mock.call_count == 1
