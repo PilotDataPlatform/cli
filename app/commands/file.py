0d30272f8f333ff7308194d30a800e51b5509560
zhiren <colorzzr@gmail.com>
2024-08-29 16:25:17 -0400

    move the project argument in mfile move command

--- app/commands/file.py
+++ app/commands/file.py
@@ -518,7 +518,6 @@ def file_metadata_download(**kwargs):
 
 
 @click.command(name='move')
-@click.argument('project_code', type=click.STRING)
 @click.argument('src_item_path', type=click.STRING)
 @click.argument('dest_item_path', type=click.STRING)
 @click.option(
@@ -529,32 +528,30 @@ def file_metadata_download(**kwargs):
     help=file_help.file_help_page(file_help.FileHELP.FILE_MOVE_Z),
     show_default=False,
 )
-# @click.option(
-#     '-y',
-#     '--yes',
-#     default=False,
-#     required=False,
-#     is_flag=True,
-#     help=file_help.file_help_page(file_help.FileHELP.FILE_MOVE_Y),
-#     show_default=True,
-# )
 @require_valid_token()
 @doc(file_help.file_help_page(file_help.FileHELP.FILE_MOVE))
 def file_move(**kwargs):
-    project_code = kwargs.get('project_code')
+    # src_item_path and dest_item_path are composed of project_code and folder_name
     src_item_path = kwargs.get('src_item_path').strip('/')
     dest_item_path = kwargs.get('dest_item_path').strip('/')
     zone = kwargs.get('zone')
 
-    if len(src_item_path.split('/')) <= 2 or len(dest_item_path.split('/')) <= 1:
+    src_project_code, src_item = src_item_path.split('/', 1)
+    dest_project_code, dest_item = dest_item_path.split('/', 1)
+    if src_project_code != dest_project_code:
+        message_handler.SrvOutPutHandler.move_action_failed(
+            src_item_path, dest_item_path, 'Cannot move files between different projects'
+        )
+        exit(1)
+    elif len(src_item.split('/')) <= 2 or len(dest_item.split('/')) <= 1:
         message_handler.SrvOutPutHandler.move_action_failed(
             src_item_path, dest_item_path, 'Cannot move root/name/shared folders'
         )
         exit(1)
 
     # tranlate keyword to correct object path
-    src_keyword, src_path = src_item_path.split('/', 1)
-    dest_keyword, dest_path = dest_item_path.split('/', 1)
+    src_keyword, src_path = src_item.split('/', 1)
+    dest_keyword, dest_path = dest_item.split('/', 1)
 
     src_type = ItemType.get_type_from_keyword(src_keyword)
     dest_type = ItemType.get_type_from_keyword(dest_keyword)
@@ -563,7 +560,7 @@ def file_move(**kwargs):
     dest_path = dest_type.get_prefix_by_type() + dest_path
 
     zone = get_zone(zone) if zone else AppConfig.Env.green_zone.lower()
-    file_meta_client = FileMoveClient(zone, project_code, src_path, dest_path)
+    file_meta_client = FileMoveClient(zone, src_project_code, src_path, dest_path)
     file_meta_client.move_file()
 
     message_handler.SrvOutPutHandler.move_action_success(src_item_path, dest_item_path)
