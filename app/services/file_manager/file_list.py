ed9d46dab17b988671aefc928d1ad7e22b36ca48
zhiren <colorzzr@gmail.com>
2024-10-07 17:19:46 -0400

    add new options to list files in trashbin

--- app/services/file_manager/file_list.py
+++ app/services/file_manager/file_list.py
@@ -6,9 +6,11 @@ from typing import Tuple
 
 import click
 import questionary
+from httpx import HTTPStatusError
 
 import app.services.logger_services.log_functions as logger
 from app.configs.app_config import AppConfig
+from app.models.item import ItemStatus
 from app.models.item import ItemType
 from app.models.service_meta_class import MetaService
 from app.services.clients.base_auth_client import BaseAuthClient
@@ -23,35 +25,48 @@ class SrvFileList(BaseAuthClient, metaclass=MetaService):
         super().__init__(AppConfig.Connections.url_bff)
 
         self.endpoint = AppConfig.Connections.url_bff + '/v1'
+        self.zone_map = {
+            0: AppConfig.Env.green_zone,
+            1: AppConfig.Env.core_zone,
+        }
 
     @require_valid_token()
-    def list_files(self, paths, zone, page, page_size) -> Tuple[str, int]:
+    def list_files(self, paths: str, zone: str, page: int, page_size: int) -> Tuple[str, int]:
         # path is formatted as <project_code>/<root_folder>/<folder1>
         # split the path in to project_code, root_folder, and folder1
         project_path = paths.strip('/').split('/')
         project_code, source_type = project_path[0], 'project'
         folder_rel_path = '/'.join(project_path[1:])
+        root_folder = ItemType.ROOTFOLDER
         if len(project_path) > 1:
             root_folder = ItemType.get_type_from_keyword(project_path[1])
             folder_rel_path = folder_rel_path.replace(project_path[1], root_folder.get_prefix_by_type()[:-1], 1)
+            folder_rel_path = folder_rel_path.lstrip('/')
 
         # now query the backend to get the file list
+        status = ItemStatus.TRASHED if root_folder == ItemType.TRASH else ItemStatus.ACTIVE
+        zone = '' if root_folder == ItemType.TRASH else zone
         params = {
             'project_code': project_code,
-            'folder': folder_rel_path,
             'source_type': source_type,
+            'folder': folder_rel_path,
             'zone': zone,
             'page': page,
             'page_size': page_size,
+            'status': str(status),
         }
-        response = self._get(f'{project_code}/files/query', params=params)
-        res_json = response.json()
-        if res_json.get('code') == 403 and res_json.get('error_msg') != 'Folder not exist':
-            SrvErrorHandler.customized_handle(ECustomizedError.PERMISSION_DENIED, True)
-        elif res_json.get('error_msg') == 'Folder not exist':
-            SrvErrorHandler.customized_handle(ECustomizedError.INVALID_FOLDER, True)
-        res = res_json.get('result')
+        try:
+            response = self._get(f'{project_code}/files/query', params=params)
+            res_json = response.json()
+        except HTTPStatusError as e:
+            response = e.response
+            res_json = response.json()
+            if response.status_code == 403 and res_json.get('error_msg') != 'Folder not exist':
+                SrvErrorHandler.customized_handle(ECustomizedError.PERMISSION_DENIED, True)
+            elif res_json.get('error_msg') == 'Folder not exist':
+                SrvErrorHandler.customized_handle(ECustomizedError.INVALID_FOLDER, True)
 
+        res = res_json.get('result')
         # then format the console output
         files, folders = '', ''
         for f in res:
@@ -59,6 +74,11 @@ class SrvFileList(BaseAuthClient, metaclass=MetaService):
             # if there is space within the nane add double quotation to aviod confusion
             if ' ' in f.get('name'):
                 f['name'] = f'"{f.get("name")}"'
+
+            # formating zone info for trashed items
+            if f.get('status') == ItemStatus.TRASHED:
+                zone = self.zone_map.get(f.get('zone'))
+                f['name'] = f'{f.get("name")}({zone})'
 
             if item_type == ItemType.FILE:
                 files = files + f.get('name') + ' ...'
